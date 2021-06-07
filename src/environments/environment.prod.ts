3bb407fdc471e2ba5542adc7be9c8ff8895a5ce6
Gayathri Rajendran <gayathri.rajendran@intellectdesign.com>
2021-06-08 03:39:35 +0530

    adds firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC1S3vqsjss2XQcHrr0Ho0L77HqHrJU-V8',
+    authDomain: 'quiz-a3ef8.firebaseapp.com',
+    projectId: 'quiz-a3ef8',
+    storageBucket: 'quiz-a3ef8.appspot.com',
+    messagingSenderId: '362391273069',
+    appId: '1:362391273069:web:fc7e52fe1db1a94a444964',
+    measurementId: 'G-T7KP1K9LPF'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC1S3vqsjss2XQcHrr0Ho0L77HqHrJU-V8',
+    authDomain: 'quiz-a3ef8.firebaseapp.com',
+    projectId: 'quiz-a3ef8',
+    storageBucket: 'quiz-a3ef8.appspot.com',
+    messagingSenderId: '362391273069',
+    appId: '1:362391273069:web:fc7e52fe1db1a94a444964',
+    measurementId: 'G-T7KP1K9LPF'
+  }
 };
 
 /*
